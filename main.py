9c74ae3b14be08a77183caf7c6d17879c4cd3e8c
breshmashri <73825231+breshmashri@users.noreply.github.com>
2021-10-21 08:29:51 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from flask.templating import render_template_string
+from data import data
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({
+        "data": data,
+        "message": "success"
+    })
+
+@app.route("/planet")
+def planet():
+    name = request.args.get("name")
+    planet_data = next(item for item in data if item["name"] == name)
+    return jsonify({
+        "data": planet_data,
+        "message": "success"
+    })
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
